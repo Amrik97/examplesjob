1d1eec453faa2a9193636a4043b1c00015ff4cf7
Amirkhan <amirhan75@gmail.com>
2023-01-20 11:46:14 +0600

    gitifelse005

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Напишите свое имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{ 
+    Console.WriteLine("Hey, Masha, how are you?");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
