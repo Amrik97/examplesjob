b11a0dd58dc59312d49a5f368464df7f6925c8de
Amirkhan <amirhan75@gmail.com>
2023-01-21 15:55:29 +0600

    commitedarray10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 62, 53, 4, 18, 5, 36, 27, 18};
+
+int n = array.Length;
+int find = 1;
+
+int index = 0;
+
+while(index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1
+    index++;
+}
